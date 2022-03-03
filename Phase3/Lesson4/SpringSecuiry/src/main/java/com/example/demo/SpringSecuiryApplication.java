87978973a2bbd3425bbcd51327791b3ebb2e82a7
Prateek Srivastava <srivastavaprateek651@.com>
2022-03-03 22:40:15 +0530

    Lesson4

---
+++ Phase3/Lesson4/SpringSecuiry/src/main/java/com/example/demo/SpringSecuiryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuiryApplication.class, args);
+	}
+
+}
