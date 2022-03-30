3d28c49e963e72fa4a97e9ddbdc4a3b04ce3df29
Prateek Srivastava <srivastavaprateek651@.com>
2022-03-30 14:42:08 +0530

    CI-CD_Deployment_Spring_Boot_Application

---
+++ Phase5/Phase5-Project/CICD Deployment for Springboot Application/src/main/java/com/aws/spring/SpringAwsApplication.java
@@ -0,0 +1,19 @@
+package com.aws.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAwsApplication {
+	@GetMapping("/")
+	public String home() {
+		return "CI/CD Deployment For Spring Boot Application By Prateek Srivastava";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsApplication.class, args);
+	}
+
+}
